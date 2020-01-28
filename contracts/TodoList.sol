43f4ff52608a786cc59e03ebaa0ad6db4d2eec84
nikhil <nikhilkumargautam01@gmail.com>
2020-01-28 13:58:37 +0530

    done

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping(uint => Task)public tasks;
+
+    constructor() public {
+        createTask("Made Todo app using ethereum");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
 }
